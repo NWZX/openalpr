dc0a25fa79a7c5e123e224c3cca03aa7269b41f4
Ruddy Varsovie <nwz44@outlook.com>
2017-05-29 20:02:42 -0400

    Fix regex not found anything

--- html/index.php
+++ html/index.php
@@ -8,8 +8,10 @@ if (isset ($_FILES['image']) and $_FILES['image']['error'] == 0) {
         if (in_array($ext, $ext_trust)) {
             move_uploaded_file($_FILES['image']['tmp_name'], 'upload/'.basename($_FILES['image']['name']));
             exec('alpr -c eu upload/'.basename($_FILES['image']['name']).' 2>&1', $output);
-            if (preg_match("\s+- [A-Z1-9]+\s+confidence: [1-9.]+", $output[3])) {
-                $plate = preg_replace("\s+- ([A-Z1-9]+)\s+confidence: [1-9.]+", "{\"Plate\":\"$1\"}", $output[3]);
+            $re = '/-.+\sconfidence:\s[0-9.]+/i';
+            $re2 = '/-(.+)\sconfidence:\s[0-9.]+/i';
+            if (preg_match($re, $output[2])) {
+                $plate = preg_replace($re2, "{\"Plate\":\"$1\"}", $output[2]);
                 echo $plate;
             }
             else {
